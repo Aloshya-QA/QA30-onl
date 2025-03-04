c5c99a3fabffb094e55db6a700466cdf2b08e255
ALIAKSEI LOKHACH <alexis.belyakoff@yandex.ru>
2025-03-04 21:36:10 +0300

    Add new functional

--- src/qa/java/lesson2/MyBMICalculator.java
+++ src/qa/java/lesson2/MyBMICalculator.java
@@ -4,22 +4,53 @@ import java.util.Scanner;
 
 public class MyBMICalculator {
     public static void main(String[] args) {
-
+        introduceBMICalculator();
     }
 
     public static void introduceBMICalculator(){
         System.out.println("Welcome to BMI calculator. It counts proportion of your height and weight.");
         System.out.println("You will be able to enter a value of your height in cm, your weight in kg.");
+        resultOfCalcData();
     }
 
-    public static void getData(){
+    public static int getHeight(){
         System.out.print("Please, enter your height in cm: ");
         int height = inputData().nextInt();
+
+        return height;
+    }
+
+    public static double getWeight() {
         System.out.print("Please, enter your weight in kg: ");
         double weight = inputData().nextDouble();
+
+        return weight;
+    }
+
+    public static double convertToMeters(int height) {
+        return (double) height / 100;
     }
 
     public static Scanner inputData(){
         return new Scanner(System.in);
+    }
+
+    public static double calcData(){
+        double height = convertToMeters(getHeight());
+        double massIndex = getWeight() / Math.pow(height, 2);
+        System.out.println("Your mass index: " + massIndex);
+        return massIndex;
+    }
+
+    public static void resultOfCalcData() {
+        double massIndex = calcData();
+        boolean normalMassIndex = 18.5 < massIndex & massIndex < 24.99;
+        if (normalMassIndex) {
+            System.out.println("Your body is fine");
+        } else if (massIndex <= 18.5) {
+            System.out.println("Underweight");
+        } else {
+            System.out.println("Overweight");
+        }
     }
 }
