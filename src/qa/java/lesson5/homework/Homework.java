c743315374989d985eb0790b5ba712e0145f09dd
ALIAKSEI LOKHACH <alexis.belyakoff@yandex.ru>
2025-03-18 19:42:13 +0300

    Have done task 1, 2

--- src/qa/java/lesson5/homework/Homework.java
+++ src/qa/java/lesson5/homework/Homework.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Homework {
     public static void main(String[] args) {
 
-//        increaseArrayValues(createIntArray());
+        increaseArrayValues(createIntArray());
         getChessBoard(8, 8);
 
     }
