e884f51891a72a086402a218ae193f51094ce85d
ALIAKSEI LOKHACH <alexis.belyakoff@yandex.ru>
2025-03-29 17:03:46 +0300

    Have done task 1

---
+++ src/qa/java/lesson8/homework/Task1/Shapes/Rectangle.java
@@ -0,0 +1,20 @@
+package qa.java.lesson8.homework.Task1.Shapes;
+
+public class Rectangle extends Shape {
+    private final double width, height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
---
+++ src/qa/java/lesson8/homework/Task1/Shapes/Triangle.java
@@ -0,0 +1,22 @@
+package qa.java.lesson8.homework.Task1.Shapes;
+
+public class Triangle extends Shape {
+    private final double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double getArea() {
+        double p = getPerimeter() / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    @Override
+    public double getPerimeter() {
+        return a + b + c;
+    }
+}
