329229aeaba2fd568cfa2e5a79fcb5f91c21d485
ALIAKSEI LOKHACH <alexis.belyakoff@yandex.ru>
2025-04-02 18:44:03 +0300

    Have done task 5

---
+++ src/qa/java/lesson10/Task5/FormatString.java
@@ -0,0 +1,21 @@
+package qa.java.lesson10.Task5;
+
+public class FormatString {
+    public static void main(String[] args) {
+        formatString("Hello world!");
+    }
+
+    public static void formatString(String str) {
+        char[] newStr = str.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for (char c : newStr) {
+            if (c != ' ') {
+                result.append(c);
+                result.append(c);
+            } else result.append(c);
+        }
+
+        System.out.println(result);
+    }
+}
