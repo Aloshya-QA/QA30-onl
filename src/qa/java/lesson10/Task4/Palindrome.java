cd7f05b09eaf2c9efbbc6a26fd69f6885679b265
ALIAKSEI LOKHACH <alexis.belyakoff@yandex.ru>
2025-04-02 18:06:30 +0300

    Have done task 4

---
+++ src/qa/java/lesson10/Task4/Palindrome.java
@@ -0,0 +1,28 @@
+package qa.java.lesson10.Task4;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+
+        isPalindrome("Я иду с мечем судия");
+
+
+    }
+
+    public static void isPalindrome(String str) {
+        String[] arrayStr = str.toLowerCase().split(" ");
+
+        System.out.print("Enter the word number in the line: ");
+        int index = new Scanner(System.in).nextInt();
+
+        StringBuilder newStr = new StringBuilder(arrayStr[index]);
+
+        String result = String.valueOf(newStr.reverse()).equals(arrayStr[index]) ?
+                "The selected word '" + arrayStr[index] + "' in the string is a palindrome" :
+                "The selected word '" + arrayStr[index] + "' in the string is not a palindrome.";
+
+        System.out.println(result);
+
+    }
+}
