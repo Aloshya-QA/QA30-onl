ba01f9ea7d7724ce4d88df25d3e1732de8efb86a
ALIAKSEI LOKHACH <alexis.belyakoff@yandex.ru>
2025-04-02 18:59:00 +0300

    Started task 3

---
+++ src/qa/java/lesson10/Task3/FindTheWord.java
@@ -0,0 +1,12 @@
+package qa.java.lesson10.Task3;
+
+public class FindTheWord {
+    public static void main(String[] args) {
+        findTheWord("fffff ab f 1234 jkjk");
+    }
+
+    public static void findTheWord(String str) {
+
+
+    }
+}
