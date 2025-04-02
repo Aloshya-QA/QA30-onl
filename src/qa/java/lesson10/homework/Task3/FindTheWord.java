bbdc1a07327a8acf9b61dc2f4ad9f3718b7b8e05
ALIAKSEI LOKHACH <alexis.belyakoff@yandex.ru>
2025-04-03 00:14:40 +0300

    Have done task 3

--- src/qa/java/lesson10/homework/Task3/FindTheWord.java
+++ src/qa/java/lesson10/homework/Task3/FindTheWord.java
@@ -2,30 +2,35 @@ package qa.java.lesson10.homework.Task3;
 
 public class FindTheWord {
     public static void main(String[] args) {
-        findTheWord("fffff abbbbbb f 1234 jkjk");
+
+        findTheWord("ffaff bf bbf bf abc aa");
     }
 
     public static void findTheWord(String str) {
         String[] newStr = str.split(" ");
 
-        int counter = 0;
-        int counter2 = 0;
+        int prevCounter = 0;
+        int nextCounter = 100;
+        int wordIndex = 0;
+
         for (int i = 0; i < newStr.length; i++) {
             for (int j = 0; j < newStr[i].length(); j++) {
                 for (int l = 0; l < newStr[i].length(); l++) {
-                    if (newStr[i].charAt(j) == newStr[i].charAt(l)) {
-                        counter++;
+                    if (newStr[i].charAt(j) != newStr[i].charAt(l)) {
+                        prevCounter++;
                     }
                 }
-                if (counter > counter2) {
-                    counter2 = counter;
-                }
-                counter = 0;
-
             }
+
+            if (prevCounter < nextCounter) {
+                nextCounter = prevCounter;
+                wordIndex = i;
+            }
+
+            prevCounter = 0;
         }
 
-        System.out.println(counter2);
+        System.out.println("A word in which the number of different characters is minimal: " + newStr[wordIndex]);
     }
 
 }
